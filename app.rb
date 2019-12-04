51fb5ac17f17d69811ca7e0277623af244180d83
James Scalise <jamescscalise@gmail.com>
2019-12-04 21:31:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -38,6 +38,7 @@ class App < Sinatra::Base
 
   get '/set_session' do
     #set session id here
+    session[:id] = 1
 
     if session[:id] == 1
       # "Session ID set. It's currently set to #{session[:id]}."
@@ -52,7 +53,7 @@ class App < Sinatra::Base
   end
 
   get '/logout' do
-    #clear session hash here
+    session.clear
     "Session has now been cleared. session content: #{session.inspect}. Continue on to the '/finish' line!"
   end
 
